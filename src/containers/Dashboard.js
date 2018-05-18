f4294caa0bf7240ffeffc26dde9dd4a3c6c4b3ce
sudogroot <sudogroot@gmail.com>
2018-05-18 10:14:05 +0100

    getting startted

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Dropdown, Segment } from 'semantic-ui-react'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        getting started
+      </div>
+    )
+  }
+}
+
+export default  Dashboard;
